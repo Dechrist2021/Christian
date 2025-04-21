6842583c4b0d7ead4b362f9b9344ad9f4cd3f4ae
Christian Mulomba <92237675+Dechrist2021@users.noreply.github.com>
2025-04-21 13:33:15 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,33 +1,18 @@
 #!/bin/bash
-# Google Chrome installation for Streamlit Cloud
-sudo apt-get update -qq
-sudo apt-get install -yqq \
-    unzip \
-    xvfb \
-    libxi6 \
-    libgconf-2-4 \
-    libnss3 \
-    libxss1 \
-    libasound2 \
-    fonts-liberation \
-    libappindicator1 \
-    xdg-utils
+# Install Chrome for Streamlit Cloud
+sudo apt-get update
+sudo apt-get install -y wget
 
-# Install Chrome (specific version that matches chromedriver)
-wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt-get install -f -yqq
+sudo apt-get install -y -f
 rm google-chrome-stable_current_amd64.deb
 
-# Install matching chromedriver
-CHROME_VERSION=$(google-chrome --version | awk '{print $3}' | cut -d'.' -f1)
-CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
-wget -q "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
-unzip -q chromedriver_linux64.zip
-rm chromedriver_linux64.zip
+# Install Chromedriver (match to Chrome version)
+CHROME_MAJOR_VERSION=$(google-chrome --version | awk '{print $3}' | cut -d'.' -f1)
+CHROMEDRIVER_VERSION=$(wget -qO- https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_MAJOR_VERSION)
+wget https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin/chromedriver
 sudo chmod +x /usr/bin/chromedriver
-
-# Verify installations
-google-chrome --version
-chromedriver --version
+rm chromedriver_linux64.zip
